1599cb1492b080ddebd44b2b4f5f08027c943f35
Vikash Kumar <vikashtech0089@gmail.com>
2025-06-24 11:09:09 +0530

    Add files via upload

---
+++ 3.MemeGeneratorApp/src/App.js
@@ -0,0 +1,15 @@
+
+import Footer from './components/Footer';
+import MemeGenerator from './components/MemeGenerator';
+
+function App() {
+  return (
+    <div>
+      <MemeGenerator />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
